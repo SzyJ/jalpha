0c8ffde3ce48aecb5a2e49a2fe44ba04a4460c20
SzyJ <szy.jackiewicz@gmail.com>
2019-06-22 14:26:58 +0100

    Add port argument and update README

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Japanese-Alphabet
 hiragana and katakana tables displayed in your terminal!
+
+## Building and Running the server
+Build and run the server:
+```
+go build && ./jalpha
+```
+
+By default, the server will start on port 8080, this can be changed with an argument like so
+```
+./jalpha <Your_Port_Here>
+```
+
+## Usage
+In these examples, it is assumed that the server has been started on localhost on port 8080
+
+To print hiragana table:
+```
+curl localhost:8080/hira
+```
+
+To print katakana table:
+```
+curl localhost:8080/kata
+```
+
+To search for a certain character, with romanji, the previous commands can be appended with:
+```
+/s/<Romanji>
+```
+
+For example to see the shi hiragana character,
+```
+curl localhost:8080/hira/s/shi
+```
+will return
+```
+shi: し
+```
+
+If multiple characters fit the search criteria, they are both returned:
+```
+ji: ジ or ヂ
+```
+
+## Future Additions
+* Expand alphabet to include Digraphs (e.g. kya: きゃ)
+* Extend search feature to chars in arbitrary string to japanese alphabet (e.g. shumon -> シュモン)
--- main.go
+++ main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
+	"./handlers"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
-	"./handlers"
+	"os"
+	"strconv"
 )
 
+var DEFAULT_PORT string = "8080"
+
+func getValidPort() string {
+	var port string
+	arguments := os.Args[1:]
+
+	if len(arguments) == 0 {
+		port = DEFAULT_PORT
+	} else {
+		port = arguments[0]
+		validPort, err := strconv.Atoi(port)
+
+		if err != nil || validPort < 1 || validPort > 65535 {
+			fmt.Println("Invalid port received. Using default.")
+			port = DEFAULT_PORT
+		}
+	}
+
+	return port
+}
+
 func main() {
+	port := getValidPort()
+
 	router := mux.NewRouter().StrictSlash(true)
 
-    router.HandleFunc("/hira", handlers.PrintHiraTable)
+	router.HandleFunc("/hira", handlers.PrintHiraTable)
 	router.HandleFunc("/hira/s", handlers.SearchHiraganaUsage)
 	router.HandleFunc("/hira/s/{romanji}", handlers.SearchHiraganaCharacter)
 
-    router.HandleFunc("/kata", handlers.PrintKataTable)
+	router.HandleFunc("/kata", handlers.PrintKataTable)
 	router.HandleFunc("/kata/s", handlers.SearchKatakanaUsage)
 	router.HandleFunc("/kata/s/{romanji}", handlers.SearchKatakanaCharacter)
 
-	fmt.Println("Server start...")
-    log.Fatal(http.ListenAndServe(":8081", router))
+	fmt.Print("Server started on port " + port + "...")
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
